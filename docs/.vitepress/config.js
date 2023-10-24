c733b208b0c0ec073f2a6b3c56ff0d1d8537ffa4
Okan YILDIRIM <63511519+ragokan@users.noreply.github.com>
2023-10-24 23:20:24 +0300

    add base

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: process.env.IS_VERCEL ? "/" : "/bunicorn/",
   title: "Bunicorn",
   description:
     "Bunicorn - Fast and type safe Bun, Deno and Edge backend framework!",
